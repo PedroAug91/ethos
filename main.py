ea34cafec1959bb9fea064d661b1ea41bf564dff
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-11-04 14:47:43 -0300

    testenovo

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+import json
+import requests
 
 class Apps(MDApp):
+
+    url = "https://datateste-e5171-default-rtdb.firebaseio.com/.json"
+
+    def patch(self, JSON):
+        to_database = json.loads(JSON)
+        requests.patch(url = self.url, json = to_database)
+
     def build(self):
         return Builder.load_file("main.kv")
-
     
 if __name__ == '__main__':
     Apps().run()
+
+#{"Parent":{"Child1": "Value", "Child2": "Value"}}
