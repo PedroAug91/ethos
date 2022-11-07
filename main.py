c53de79b1fef880250795ab44e8316879b68b5cb
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-11-07 08:47:52 -0300

    testando

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 import json
 import requests
+<<<<<<< Updated upstream
 
 class Apps(MDApp):
 
@@ -13,6 +14,19 @@ class Apps(MDApp):
 
     def build(self):
         return Builder.load_file("main.kv")
+=======
+
+
+class Apps(MDApp):
+    url = 'https://datateste-e5171-default-rtdb.firebaseio.com/.json'
+    def build(self):
+        return Builder.load_file("main.kv")
+
+    def patch(self, JSON):
+        to_database = json.loads(JSON)
+        requests.patch(url = self.url, json = to_database)
+
+>>>>>>> Stashed changes
     
 if __name__ == '__main__':
     Apps().run()
