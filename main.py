4635fdf50f82033935e7a74c3483f2c6f039be6d
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-11-11 17:05:07 -0300

    FOIIII!!!!

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from firebase import firebase
 import json
 import requests
 
 class Apps(MDApp):
 
-    def send_data(self, name, email, password):
-        from firebase import firebase
+    check = False
 
+    def send_data(self, name, email, password):
         firebase = firebase.FirebaseApplication('https://testando-ae5b2-default-rtdb.firebaseio.com/', None)
 
         data = {
@@ -17,6 +18,16 @@ class Apps(MDApp):
         }
 
         firebase.post('https://testando-ae5b2-default-rtdb.firebaseio.com/Users', data)
+    
+    def verify_data(self, email, password):     
+        firebase = firebase.FirebaseApplication('https://testando-ae5b2-default-rtdb.firebaseio.com/', None)
+        result = firebase.get('https://testando-ae5b2-default-rtdb.firebaseio.com/Users', '')
+
+        for i in result.keys():
+            if result[i]['Email'] == email:
+                if result[i]['Password'] == password:
+                    self.check = True
+
 
     def build(self):
         return Builder.load_file("main.kv")
